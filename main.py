e9aa7dba654719a5ee0498052d0d8e52d5f219d5
naga-naga <kiyoharu.639721@gmail.com>
2021-07-15 20:26:31 +0900

    [add] 画面上にテキストを流せるようになった

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import PySimpleGUI as sg
+import random
+
+screen_size = sg.Window.get_screen_size()
+
+def draw_comments(graph, comments_list):
+    for comment in comments_list:
+        graph.draw_text(comment, (screen_size[0], random.randint(0, screen_size[1])), color="white", font=("meiryo", 24))
 
 def main():
-    screen_size = sg.Window.get_screen_size()
-
     graph_elem = sg.Graph(canvas_size=screen_size, graph_bottom_left=(0, screen_size[1]), graph_top_right=(screen_size[0], 0), key="-GRAPH-", background_color="green")
     layout1 = [[graph_elem]]
     window1 = sg.Window("Flowing Comments", layout1, location=(0, 0), keep_on_top=True, element_padding=(0, 0), margins=(0, 0), no_titlebar=True, finalize=True, transparent_color="green")
@@ -14,9 +19,15 @@ def main():
     window2 = sg.Window("controller", layout2, keep_on_top=True, grab_anywhere=True, no_titlebar=True, finalize=True)
 
     while True:
-        window, event, value = sg.read_all_windows(timeout=0)
+        window, event, value = sg.read_all_windows(timeout=10)
         if event in (sg.WINDOW_CLOSED, "Exit"):
             break
+
+        if event == "Send":
+            draw_comments(graph_elem, [value["-INPUT-"]])
+
+        # 画面上のテキストを動かす
+        graph_elem.move(-4, 0)
 
     window1.close()
     window2.close()

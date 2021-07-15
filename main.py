a864a52d789efb3fe70d7b5897615dfb6fb961a3
naga-naga <kiyoharu.639721@gmail.com>
2021-07-15 19:29:23 +0900

    [style] メイン処理をmain()に入れた

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import PySimpleGUI as sg
 
-screen_size = sg.Window.get_screen_size()
+def main():
+    screen_size = sg.Window.get_screen_size()
 
-graph_elem = sg.Graph(canvas_size=screen_size, graph_bottom_left=(0, screen_size[1]), graph_top_right=(screen_size[0], 0), key="-GRAPH-", background_color="green")
-layout1 = [[graph_elem]]
-window1 = sg.Window("Flowing Comments", layout1, location=(0, 0), keep_on_top=True, element_padding=(0, 0), margins=(0, 0), no_titlebar=True, finalize=True, transparent_color="green")
+    graph_elem = sg.Graph(canvas_size=screen_size, graph_bottom_left=(0, screen_size[1]), graph_top_right=(screen_size[0], 0), key="-GRAPH-", background_color="green")
+    layout1 = [[graph_elem]]
+    window1 = sg.Window("Flowing Comments", layout1, location=(0, 0), keep_on_top=True, element_padding=(0, 0), margins=(0, 0), no_titlebar=True, finalize=True, transparent_color="green")
 
-layout2 = [
-    [sg.Text("コメント"), sg.Input(key="-INPUT-"), sg.Button("Send")],
-    [sg.Button("Exit")],
-]
-window2 = sg.Window("controller", layout2, keep_on_top=True, grab_anywhere=True, no_titlebar=True, finalize=True)
+    layout2 = [
+        [sg.Text("コメント"), sg.Input(key="-INPUT-"), sg.Button("Send")],
+        [sg.Button("Exit")],
+    ]
+    window2 = sg.Window("controller", layout2, keep_on_top=True, grab_anywhere=True, no_titlebar=True, finalize=True)
 
-while True:
-    window, event, values = sg.read_all_windows(timeout=0)
-    if event in (sg.WINDOW_CLOSED, "Exit"):
-        break
+    while True:
+        window, event, value = sg.read_all_windows(timeout=0)
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            break
 
-window1.close()
-window2.close()
+    window1.close()
+    window2.close()
+
+if __name__ == "__main__":
+    main()
